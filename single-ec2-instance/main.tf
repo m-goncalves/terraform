904076e3abf5df40abe982bf1d367c953d33ab39
Marcelo Goncalves <tchelojg@gmail.com>
2020-12-21 20:30:31 -0300

    Commenting single instance terraform deployment

--- single-ec2-instance/main.tf
+++ single-ec2-instance/main.tf
@@ -2,16 +2,19 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "blur-instance" {
+resource "aws_instance" "single-instance" {
+    # Specifies the image to be used.
     ami                 = "ami-0c55b159cbfafe1f0"
     instance_type       = "t2.micro"
     #tells the ec2 instance to use the security group. It creates an implicit dependency
     # value inside brackets dont need "" . Why?
-    vpc_security_group_ids = [aws_security_group.blur-security-group.id]
+    vpc_security_group_ids = [aws_security_group.si-security-group.id]
     
     tags = {
-      "Name" = "blur-instance"
+      "Name" = "single-instance"
     }
+
+    # "<<-EOF ... EOF" makes possible to use multi-line strings. 
     user_data = <<-EOF
                 #!/bin/bash
                 echo "hello, World!"> index.html
@@ -19,23 +22,23 @@ resource "aws_instance" "blur-instance" {
                 EOF
 }
 
-# necessary to allow incoming traffic
-resource "aws_security_group" "blur-security-group" {
-    name = "blur-instance"
+# Necessary to allow incoming traffic.
+resource "aws_security_group" "si-security-group" {
+    name = "single-instance"
     ingress {
         from_port = var.server_port
         to_port = var.server_port
         protocol = "tcp"
-        #cidr_blocks allow to specify IP adress ranges
+        #cidr_blocks is required to specify IP adress ranges.
         cidr_blocks = ["0.0.0.0/0"]
     }
   
 }
 
 output "public_ip" {
-    value           = aws_instance.blur-instance.public_ip
+    value           = aws_instance.single-instance.public_ip
     description     = "The public IP of the web server!" 
-    # important to instruct terraform not to log sensitive data
+    # Can be used to instruct terraform not to log sensitive data
     sensitive = false
   
 }
