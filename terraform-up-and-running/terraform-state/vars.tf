78ae6f270d12eac3e353dfc69347ead7e021d650
marcelo goncalves <tchelojg@gmail.com>
2022-10-12 15:03:23 +0200

    updating gitignore

--- terraform-up-and-running/terraform-state/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  type    = string
-  default = "AKIAVVFY676JTCUC6AHI"
-
-}
-variable "secret_key" {
-  type    = string
-  default = "vx9Fjs9Cn31NQmKKYwjfDbTfH911uMEclhNDuR/y"
-}
