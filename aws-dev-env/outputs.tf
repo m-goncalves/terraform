4b7352f5bba6cfc66e939de0c87a60b0db3dc98c
marcelo goncalves <tchelojg@gmail.com>
2022-10-12 15:12:47 +0200

    untrucking files

--- aws-dev-env/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "dev_instance_ip" {
-  value = aws_instance.dev_instance.public_ip
-}
--- terraform-up-and-running/terraform-state-file-isolation/stage/services/webserver-cluster/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "alb_dns_name" {
-  description = "The public IP address of the web server"
-  value       = aws_lb.cluster_webservers_lb.dns_name
-
-}
