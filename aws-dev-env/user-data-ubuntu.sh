f0e5161fa76cfa3f0dd797ee5a6e9017e165c304
marcelo goncalves <tchelojg@gmail.com>
2022-10-12 15:16:22 +0200

    updating gitignore

---
+++ aws-dev-env/user-data-ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
+wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+sudo apt update
+sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
+sudo usermod --append --groups docker "$USER"
+sudo systemctl enable docker
+
+# install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /tmp/docker-compose
+chmod +x /tmp/docker-compose
+sudo mv /tmp/docker-compose /usr/local/bin/docker-compose
